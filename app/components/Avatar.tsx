8691b60ffa4a13cb2f6726f2e804a93f555ebf0e
Mohamed <mohameda7maa@gmail.com>
2023-05-31 15:20:51 +0300

    change offline avatar shape

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -50,7 +50,24 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
             md:w-3
           "
         />
-      ) : null}
+      ) : (
+        <span
+          className="
+        absolute 
+        block 
+        rounded-full 
+        bg-gray-500
+        ring-2 
+        ring-white 
+        top-0 
+        right-0
+        h-2 
+        w-2 
+        md:h-3 
+        md:w-3
+      "
+        />
+      )}
     </div>
   );
 };
